4057ced54805d68f622df40a7e70955130224b8a
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2019-06-04 22:03:29 +0700

    Update

--- quyetde/app.js
+++ quyetde/app.js
@@ -21,24 +21,73 @@ app.get('/', (req, res) => {
 			<h1>
 				${randomQuestion.content}
 			</h1>
-			<form action="/vote" method="POST">
-				<input type="hidden" name="id" value"${randomQuestion.id}" >
-				<button type="submit" value="no" name="vote">Sai/không/trái</button>
-				<button type="submit" value="yes" name="vote">Đúng/có/phải</button>
-			</form>
-			<button>Kết quả vote</button>
-			<button>Câu hỏi khác</button>
+			<a href="/vote/${randomQuestion.id}/no"><button>Sai/không/trái</button></a>
+			<a href="/vote/${randomQuestion.id}/yes"><button>Đúng/có/phải</button></a>
+			<a href="/question/${randomQuestion.id}"><button>Kết quả vote</button></a>
+			<a href="/"><button>Câu hỏi khác</button></a>
 		`);
+
+		// <form action="/vote" method="POST">
+		// 	<input type="hidden" name="id" value="${randomQuestion.id}" >
+		// 	<button type="submit" value="no" name="vote">Sai/không/trái</button>
+		// 	<button type="submit" value="yes" name="vote">Đúng/có/phải</button>
+		// </form>
 	}
 	// res.sendFile(__dirname + '/views/home.html');
 });
 
 app.post('/vote', (req, res) => {
 	console.log(req.body);
+	const vote = req.body.vote;
+	const id = req.body.id;
+	const fileData = fs.readFileSync('questions.txt', { encoding: 'utf-8' });
+	let questionList = [];
+	if(fileData) {
+		questionList = JSON.parse(fileData);
+	}
+
+	// if(vote == 'no') {
+	// 	questionList[id].no += 1;
+	// } else {
+	// 	questionList[id].yes += 1;
+	// }
+	questionList[id][vote] += 1;
+
+	fs.writeFileSync('questions.txt', JSON.stringify(questionList));
+	res.redirect('/');
 });
 
 app.get('/ask', (req, res) => {
+	console.log(req.query);
 	res.sendFile(__dirname + '/views/ask.html');
+});
+
+// /vote/3/yes
+app.get('/vote/:questionId/:vote', (req, res) => {
+	const questionId = req.params.questionId;
+	const vote = req.params.vote;
+	const fileData = fs.readFileSync('questions.txt', { encoding: 'utf-8' });
+	let questionList = [];
+	if(fileData) {
+		questionList = JSON.parse(fileData);
+	}
+	questionList[questionId][vote] += 1;
+
+	fs.writeFileSync('questions.txt', JSON.stringify(questionList));
+	res.redirect(`/question/${questionList[questionId].id}`);
+});
+
+app.get('/question/:questionId', (req, res) => {
+	const questionId = req.params.questionId;
+	const fileData = fs.readFileSync('questions.txt', { encoding: 'utf-8' });
+	let questionList = [];
+	if(fileData) {
+		questionList = JSON.parse(fileData);
+	}
+	const question = questionList[questionId];
+	res.send(`
+		${question.content} | yes: ${question.yes} | no: ${question.no}
+	`);
 });
 
 // app.get('/', (req, res) => {
--- quyetde/public/ask.js
+++ quyetde/public/ask.js
@@ -1,12 +1,6 @@
-const textInput = document.querySelector('textarea');
-const remainElement = document.getElementById('remain');
-
-textInput.addEventListener("input", () => {
-	const inputLength = textInput.value.length;
-	const remain = 200 - inputLength;
-	// if(remain <= 0) {
-	// 	textInput.value = textInput.value.slice(0, 200);
-	// 	remainElement.innerText = 0;
-	// } else remainElement.innerText = remain;
-	remainElement.innerText = remain;
+$( document ).ready(function() {
+	$('textarea').on("input", () => {
+		const inputLength = $('textarea').val().length;
+		$('#remain').text(200 - inputLength);
+	});
 });
